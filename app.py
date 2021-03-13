b98188ab74c7304f730416bcd0cd361df8dc88f4
Vilian <vi.gecoding@gmail.com>
2021-03-13 14:08:26 +0200

    Front end for new-ticket mostly done

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from operator import length_hint
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
-# import ticket_module
 import os
+import ticket_module
+
+# CONSTANTS
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///market.db '
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'secret!'
+HOST = "0.0.0.0"
+PORT = 80
+
+# GlOBALS
 socketio = SocketIO(app, cors_allowed_origins="*")
+tc = ticket_module.TClient()
+db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///market.db '
 
 # class Item(db.Model):
 #     id = db.Column(db.Integer(), primary_key=True)
@@ -20,11 +27,22 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 def index_page():
     return render_template('index.html')
 
+def ack():
+    print("Message was recived!")
+    
+@socketio.on("message")
+def handle_message(msg):
+    print(msg)
 
+
+@socketio.on('get_all_routes')
+def my_event():
+    print("Routes were requested")
+    socketio.emit("all_routes", tc.route_beginings())
 
 if __name__ == '__main__':
     if not os.path.exists("market.db"):
         print("Did not find a databse: Creating a new one....")
         db.create_all()
-    socketio.run(port=8000, debug=True, host='192.168.0.2')
+    socketio.run(app, port=PORT, debug=True, host=HOST)
     
--- ticket_module.py
+++ ticket_module.py
@@ -4,6 +4,7 @@ import json
 
 class TClient(object):
     def __init__(self) -> None:
+        print("Initializing route Class..")
         self.all_routes = TClient.load_all_routes()
     
 
@@ -15,28 +16,35 @@ class TClient(object):
                 all_routes.append(json.load(f))
         return all_routes
 
-    def filter(self,st1) -> list:
-        for station_route in self.all_routes:
-            if st1 in station_route["_stations"]:
-                return station_route
-        return None
-        
-    def filtered_routes(self, filter1, filter2=None):
+
+
+    def route_beginings(self) -> list:
+        """Returns a list of all only unique stations from routes excluding the last one"""
+        route_beginings_list = []
+        for route in self.all_routes:
+            for station in route["_stations"]:
+                if station not in route_beginings_list:
+                    route_beginings_list.append(station)
+        return route_beginings_list
+
+
+    def filter_routes(self, station1:str, station2:str = None) -> list or None:
         """Function return list of all routes that pass the filter: filter1 > filter2"""
         for route in self.all_routes:
             # sr = station rotue
             sr = route["_stations"]
-            if filter2:
-                if filter1 in sr and filter2 in sr:
-                    if sr.index(filter1) < sr.index(filter2): return route
+            if station2:
+                if station1 in sr and station2 in sr:
+                    if sr.index(station1) < sr.index(station2): return route
             else:
-                if filter1 in sr:
+                if station1 in sr:
                     return route
             
 
         return None
 
-y = "РУСЕ"
-z = "БЯЛА"
-x = TClient()
-print(x.filtered_routes(y,z))
+# y = "РУСЕ"
+# z = "БЯЛА"
+# x = TClient()
+# # print(x.filtered_routes(y,z))
+# print(x.route_beginings())
